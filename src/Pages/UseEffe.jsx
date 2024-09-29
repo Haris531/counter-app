2053d872dd3f732090e996ab832e7de7d33df8fa
Haris531 <sharisahmed8811@gmail.com>
2024-09-29 17:02:25 +0500

    changes

--- src/Pages/UseEffe.jsx
+++ src/Pages/UseEffe.jsx
@@ -11,11 +11,11 @@ const UseEffe = () => {
     },[num]);
 
   return (
-    <>
+    <div className='flex flex-col justify-center items-center'>
     <p className='text-3xl'>you clicked {num} time</p>
     <button onClick={incNum} className='bg-slate-500 text-xl mt-4'>Click me</button>
  
-    </>
+    </div>
   )
 }
 export default UseEffe
